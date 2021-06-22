ede9219475ea51b7313d093fbf7035320970701b
Aleff-Kelvin <aleff.stampini@bandtec.com.br>
2021-06-22 02:38:20 -0300

    criando os arquivos base

---
+++ assets/js/funcoes.js
@@ -0,0 +1,52 @@
+let login_usuario;
+let nome_usuario;
+
+function redirecionar_login() {
+    window.location.href = 'login.html';
+}
+
+function verificar_autenticacao() {
+    login_usuario = sessionStorage.login_usuario_meuapp;
+    nome_usuario = sessionStorage.nome_usuario_meuapp;
+    
+    console.log(login_usuario);
+    console.log(nome_usuario);
+
+    if (login_usuario == undefined)  {
+        redirecionar_login();
+    } else {
+        result_nome.innerHTML = nome_usuario;
+        validar_sessao();
+    }
+    
+}
+
+function logoff() {
+    finalizar_sessao();
+    sessionStorage.clear();
+    redirecionar_login();
+}
+
+function validar_sessao() {
+    login_usuario = sessionStorage.login_usuario_meuapp;
+    nome_usuario = sessionStorage.nome_usuario_meuapp;
+
+    fetch(`/usuarios/sessao/${login_usuario}`, {cache:'no-store'})
+    .then(resposta => {
+        if (resposta.ok) {
+            resposta.text().then(texto => {
+                console.log('Sessão :) ', texto);    
+            });
+        } else {
+            console.error('Sessão :.( ');
+            logoff();
+        } 
+    });    
+}
+
+function finalizar_sessao() {
+    login_usuario = sessionStorage.login_usuario_meuapp;
+    nome_usuario = sessionStorage.nome_usuario_meuapp;
+    
+    fetch(`/usuarios/sair/${login_usuario}`, {cache:'no-store'}); 
+}
